13c4d33de552c8ad1c87e907905e34a8d73ce9af
Loucypher01 <101384600+Loucypher01@users.noreply.github.com>
2022-03-11 13:58:15 +0700

    Update Program.cs

--- Project01/Project01/Program.cs
+++ Project01/Project01/Program.cs
@@ -6,7 +6,7 @@ namespace Project01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, my name is Truong Anh Thanh");
         }
     }
 }
